e93d4a04ef19b9127d4a8205a4aea683a7bea348
Roman Jasiek <roman.jasiek@avalost.de>
2021-09-20 21:55:52 +0200

    Started project, created first components and a basic styling

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+//Import styles
+import './styles/app.scss';
+// Adding components
+import Player from './components/Player';
+import Song from './components/Song';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Song />
+      <Player />
     </div>
   );
 }
