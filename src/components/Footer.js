ec875534b8d69fc8fc817d2e4f0950c4641e6979
Roman Jasiek <roman.jasiek@avalost.de>
2021-09-29 13:06:47 +0200

    Small changes to Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -7,11 +7,11 @@ const Footer = () => {
     return(
         <div>
     <ul className="footer-menu">
+        <li className="footer-menu-item">Created by <span className="footer-icon">Roman Jasiek</span> in 2021 with <FontAwesomeIcon className="footer-icon" icon={faHeart}/>, <FontAwesomeIcon className="footer-icon" icon={faCoffee}/> and incredible support from <span className="footer-icon">Ghassan Aldarwish</span> in Magdeburg</li>
         <li className="footer-menu-item"><a href="tel:+491724563446"><FontAwesomeIcon icon={faPhoneSquareAlt}/> Mobile: +49 172 456 3 446</a></li>
         <li className="footer-menu-item"><a href="mailto:romanjasiek@me.com?subject=Contact%20Request"><FontAwesomeIcon icon={faEnvelopeSquare}/> romanjasiek@me.com</a></li>
         <li className="footer-menu-item"><a href="https://www.linkedin.com/in/romanjasiek/"><FontAwesomeIcon icon={faLinkedin}/> LinkedIn</a></li>
         <li className="footer-menu-item"><a href="https://github.com/romanjasiek"><FontAwesomeIcon icon={faGithub}/> GitHub</a></li>
-        <li className="footer-menu-item">Created by <span className="footer-icon">Roman Jasiek</span> in 2021 with <FontAwesomeIcon className="footer-icon" icon={faHeart}/> and <FontAwesomeIcon className="footer-icon" icon={faCoffee}/>  in Magdeburg</li>
     </ul>
         </div>
     )
